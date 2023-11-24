0dc3c0d0fba6c2ad4bf37c0b5639a97b8ef42ed6
miltinmolecule <miltindev@gmail.com>
2023-11-24 17:01:06 +0100

    feat: Referral page UI

--- src/views/Dashboard/Referral/index.vue
+++ src/views/Dashboard/Referral/index.vue
@@ -1,11 +1,69 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="dashboard__wrapper">
-    <div class="app_card">
-      <div class="custom-layout">
-        <div class="">Hello</div>
-        <div class="">Hello</div>
-        <div class="">Hello</div>
+    <div class="grid-55-45">
+      <div class="app_card">
+        <h1>Refer and Earn</h1>
+        <p>
+          Share your referral link and earn 2% - 10% when your referrals pay or co-own an asset.
+        </p>
+      </div>
+      <div class="app_card">
+        <h3>Tell your friends</h3>
+        <ul class="social-links">
+          <li>
+            <a href="">
+              <div class="-icon"><i class="whatsapp"></i></div>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <div class="-icon"><i class="facebook"></i></div>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <div class="-icon"><i class="instagram"></i></div>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <div class="-icon"><i class="twitter"></i></div>
+            </a>
+          </li>
+        </ul>
+        <button class="btn-primary">
+          <span class="-icon mr-[10px]"><i class="copy"></i></span>
+          Copy your referral link
+        </button>
+      </div>
+    </div>
+
+    <div class="app_card m300 no-pad">
+      <div class="card-view-title pad-inner">
+        <h2>Referrals</h2>
+      </div>
+      <div class="table-list">
+        <table>
+          <thead>
+            <th>Date</th>
+            <th>Name</th>
+            <th>Email address</th>
+            <th>Phone Number</th>
+            <th>Status</th>
+            <th>Amount Earned</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>2022-03-12</td>
+              <td>Oshokoya Oyinkansola</td>
+              <td>oshokoyaoyin@gmail.com</td>
+              <td>08106744642</td>
+              <td>Active</td>
+              <td>N3,000</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
